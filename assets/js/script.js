bfb6c691b3a08c38d7a2270adde3a814d5e42b25
Igor Basuga <igor.basuga@gmail.com>
2020-01-31 14:16:06 +0000

    fix text indentation in styles and script

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,9 @@
  **/
 
 var tileSets = {
-    beginner: createSet(16),
-    intermediate: createSet(24),
-    expert: createSet(36)
+  beginner: createSet(16),
+  intermediate: createSet(24),
+  expert: createSet(36)
 };
 
 function createSet(amount) {
@@ -17,11 +17,11 @@ function createSet(amount) {
 
 // Shuffle algorithm obtained from Stack Overflow
 function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
+  for (let i = array.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * (i + 1));
+  [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
 
@@ -30,89 +30,89 @@ function shuffle(array) {
  * Solution obtained from Stack Overflow & W3Schools.
  **/
 function levelSelect(selection) {
-    localStorage.setItem("difficultyLevel", selection);
-    window.open("gameplay.html", "_self", false);
+  localStorage.setItem("difficultyLevel", selection);
+  window.open("gameplay.html", "_self", false);
 }
 
 // The gameBoard object handles tile management logic.
 var gameBoard = {
 
-    gameSet: [],
+  gameSet: [],
 
-    /** 
-     * Shuffle the original tileSet (based on difficultyLevel selection) and assign a "pairID" property to each shuffled tile, 
-     * based on its number (position) in the original tileSet. The "position" property of the tile 
-     * equals its position in the new (shuffled) array. 
-     * The resulting array is the gameSet array of tile objects.  
-     **/ 
-    generateBoard: function () {
-        var difficultyLevel = localStorage.getItem("difficultyLevel");
-        let shuffledSet = shuffle(tileSets[difficultyLevel]);
-        let tilePair;
-        for (tileNumber of shuffledSet) {
-            if (tileNumber % 2 === 0) {
-                tilePair = tileNumber / 2;
-            } else {
-                tilePair = (tileNumber + 1) / 2;
-            }
-            this.gameSet.push({
-                position: shuffledSet.indexOf(tileNumber),
-                pairID: tilePair,
-            });
-        }
-        view.displayBoard(this.gameSet);
-        view.displayMessage("Good luck!");
-        handlers.clickExpect();
-    }, 
-
-    // check if two tiles have been selected
-    isPair: function () {
-        var selectedTiles = $(".tile").not(".faceDown");
-
-        if (selectedTiles.length === 2) {
-            handlers.disableClick();
-            this.checkMatch(selectedTiles);
-        }
-    },
-
-    // check if the two selected tiles match
-    checkMatch: function (selectedTiles) {
-        let tileOne = selectedTiles[0];
-        let tileTwo = selectedTiles[1];
-
-        if (tileOne.className !== tileTwo.className) {
-            view.displayFailureMessage();
-            setTimeout(function () {
-                view.unflipTiles(tileOne, tileTwo);
-            }, 1500);
-            setTimeout(function () {
-                handlers.clickExpect();
-            }, 1600);
-        } else {
-            view.displaySuccessMessage();
-            setTimeout(function () {
-                view.removeMatchedTiles(tileOne, tileTwo);
-                gameBoard.checkBoardCleared();
-            }, 1500);
-        }
-    },
-
-    // check if all tiles have been cleared from the board
-    checkBoardCleared: function () {
-        if ($(".tile.completed").length === gameBoard.gameSet.length) {
-            view.displayMessage("Congratulations!!! Game completed!");
-            view.displayPlayAgainButton();
-        } else {
-            handlers.clickExpect();
-        }
-    },
-
-    restartGame: function () {
-        window.open("gameplay.html", "_self", false);
-        setTimeout(function () {
-            gameBoard.generateBoard();
-        }, 200);
+  /** 
+   * Shuffle the original tileSet (based on difficultyLevel selection) and assign a "pairID" property to each shuffled tile, 
+   * based on its number (position) in the original tileSet. The "position" property of the tile 
+   * equals its position in the new (shuffled) array. 
+   * The resulting array is the gameSet array of tile objects.  
+   **/ 
+  generateBoard: function () {
+  var difficultyLevel = localStorage.getItem("difficultyLevel");
+  let shuffledSet = shuffle(tileSets[difficultyLevel]);
+  let tilePair;
+  for (tileNumber of shuffledSet) {
+    if (tileNumber % 2 === 0) {
+    tilePair = tileNumber / 2;
+    } else {
+    tilePair = (tileNumber + 1) / 2;
     }
+    this.gameSet.push({
+    position: shuffledSet.indexOf(tileNumber),
+    pairID: tilePair,
+    });
+  }
+  view.displayBoard(this.gameSet);
+  view.displayMessage("Good luck!");
+  handlers.clickExpect();
+  }, 
+
+  // check if two tiles have been selected
+  isPair: function () {
+  var selectedTiles = $(".tile").not(".faceDown");
+
+  if (selectedTiles.length === 2) {
+    handlers.disableClick();
+    this.checkMatch(selectedTiles);
+  }
+  },
+
+  // check if the two selected tiles match
+  checkMatch: function (selectedTiles) {
+  let tileOne = selectedTiles[0];
+  let tileTwo = selectedTiles[1];
+
+  if (tileOne.className !== tileTwo.className) {
+    view.displayFailureMessage();
+    setTimeout(function () {
+    view.unflipTiles(tileOne, tileTwo);
+    }, 1500);
+    setTimeout(function () {
+    handlers.clickExpect();
+    }, 1600);
+  } else {
+    view.displaySuccessMessage();
+    setTimeout(function () {
+    view.removeMatchedTiles(tileOne, tileTwo);
+    gameBoard.checkBoardCleared();
+    }, 1500);
+  }
+  },
+
+  // check if all tiles have been cleared from the board
+  checkBoardCleared: function () {
+  if ($(".tile.completed").length === gameBoard.gameSet.length) {
+    view.displayMessage("Congratulations!!! Game completed!");
+    view.displayPlayAgainButton();
+  } else {
+    handlers.clickExpect();
+  }
+  },
+
+  restartGame: function () {
+  window.open("gameplay.html", "_self", false);
+  setTimeout(function () {
+    gameBoard.generateBoard();
+  }, 200);
+  }
 }
 
 // Object for managing tile click enabling and disabling.
@@ -122,69 +122,69 @@ var handlers = {
  * clickExpect function set up based on a video by Watch and Code
  * and a post on Stack Overflow.
  **/
-    clickExpect: function () {
-        $("#gameArea").children().not(".completed").on("click", function (event) {
-            let clicked = event.target;
-            if (clicked.className.includes("tile")) {
-                let position = Number.parseInt(clicked.id);
-                view.selectTile(position);
-            }
-        });
-    },
-
-    disableClick: function () {
-        $("#gameArea").children().not(".completed").off();
+  clickExpect: function () {
+  $("#gameArea").children().not(".completed").on("click", function (event) {
+    let clicked = event.target;
+    if (clicked.className.includes("tile")) {
+    let position = Number.parseInt(clicked.id);
+    view.selectTile(position);
     }
+  });
+  },
+
+  disableClick: function () {
+  $("#gameArea").children().not(".completed").off();
+  }
 }
 
 
 // Object for managing the display of tiles and game messages.
 var view = {
 
-    displayBoard: function (set) {
-        let difficulty = localStorage.getItem("difficultyLevel")
-        let gameArea = $("#gameArea");
-        for (tile of set) {
-            let tileDiv = document.createElement("div");
-            tileDiv.id = `${tile.position}`;
-            tileDiv.className = `tile ${difficulty}Pair${tile.pairID} faceDown`
-            gameArea.append(tileDiv);
-        }
-    },
+  displayBoard: function (set) {
+  let difficulty = localStorage.getItem("difficultyLevel")
+  let gameArea = $("#gameArea");
+  for (tile of set) {
+    let tileDiv = document.createElement("div");
+    tileDiv.id = `${tile.position}`;
+    tileDiv.className = `tile ${difficulty}Pair${tile.pairID} faceDown`
+    gameArea.append(tileDiv);
+  }
+  },
 
-    selectTile: function (position) {
-        $(`#${position}`).removeClass("faceDown");
-        gameBoard.isPair();
-    },
+  selectTile: function (position) {
+  $(`#${position}`).removeClass("faceDown");
+  gameBoard.isPair();
+  },
 
-    unflipTiles: function (tileOne, tileTwo) {
-        tileOne.classList.add("faceDown");
-        tileTwo.classList.add("faceDown");
-    },
+  unflipTiles: function (tileOne, tileTwo) {
+  tileOne.classList.add("faceDown");
+  tileTwo.classList.add("faceDown");
+  },
 
-    removeMatchedTiles: function (tileOne, tileTwo) {
-        tileOne.classList.add("faceDown", "completed");
-        tileTwo.classList.add("faceDown", "completed");
-    },
+  removeMatchedTiles: function (tileOne, tileTwo) {
+  tileOne.classList.add("faceDown", "completed");
+  tileTwo.classList.add("faceDown", "completed");
+  },
 
-    displayMessage: function (messageText) {
-        $("#messageArea").text(messageText);
-    },
+  displayMessage: function (messageText) {
+  $("#messageArea").text(messageText);
+  },
 
-    displaySuccessMessage: function () {
-        var messages = ["Nice!", "Good job!", "Got it!", "Super!"];
-        let messageChoice = messages[Math.round(Math.random() * 3)];
-        this.displayMessage(messageChoice);
-    },
+  displaySuccessMessage: function () {
+  var messages = ["Nice!", "Good job!", "Got it!", "Super!"];
+  let messageChoice = messages[Math.round(Math.random() * 3)];
+  this.displayMessage(messageChoice);
+  },
 
-    displayFailureMessage: function () {
-        var messages = ["Close... but no. :)", "Nope. Try again.", "Almost... but not quite. :)"];
-        let messageChoice = messages[Math.round(Math.random() * 2)];
-        this.displayMessage(messageChoice);
-    },
+  displayFailureMessage: function () {
+  var messages = ["Close... but no. :)", "Nope. Try again.", "Almost... but not quite. :)"];
+  let messageChoice = messages[Math.round(Math.random() * 2)];
+  this.displayMessage(messageChoice);
+  },
 
-    displayPlayAgainButton: function () {
-        $("#restartForm").attr("action", "levelselection.html");
-        $("#restartButton").removeAttr("onclick").attr("type", "submit").text("Play again?");
-    }
+  displayPlayAgainButton: function () {
+  $("#restartForm").attr("action", "levelselection.html");
+  $("#restartButton").removeAttr("onclick").attr("type", "submit").text("Play again?");
+  }
 }
